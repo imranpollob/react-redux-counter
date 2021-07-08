ebdef0d5de4cf3230707d95a50588b4b8410def6
Imran Pollob <polboy777@gmail.com>
2021-07-08 16:25:47 +0600

    Update counterAction.js

--- src/store/actions/counterAction.js
+++ src/store/actions/counterAction.js
@@ -12,11 +12,11 @@ export const decCounter = number => {
   };
 };
 
-export const incCounterAsync = number => {
-  return dispatch => {
-    fetch("https://wrkm8.sse.codesandbox.io/")
-      .then(res => res.json())
-      .then(res => dispatch(incCounter(res.random_number)))
-      .catch(err => console.log(err));
+export const incCounterAsync = (number) => {
+  return (dispatch) => {
+    setTimeout(
+      () => dispatch(incCounter(Math.ceil(Math.random() * 100))),
+      1000
+    );
   };
 };
